41cd03c5c225956b0ecb5884b63f1efef6c8f9b1
Shinra-Shinny <shinrazac@yahoo.com>
2021-10-24 17:37:58 -0500

    add asteroids to map

--- maps/test.gd
+++ maps/test.gd
@@ -1,7 +1,8 @@
 extends Spatial
 
 onready var hud = $HUD
+onready var galaxy_plane = $Navigation/NavMesh/Plane
 
 func _ready():
-	var world_size = Vector2(500, 500)
+	var world_size = galaxy_plane.mesh.size
 	hud.setup(world_size/2, -1*(world_size/2), Vector2(0,0))
